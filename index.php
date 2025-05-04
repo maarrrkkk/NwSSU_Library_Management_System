db8d2cca6258896893956354e6d875b1df91464a
maarrrkkk <johnmarkaguilar405@gmail.com>
2025-05-04 12:46:09 +0800

    added admin folder for admin interface

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 require_once 'components/header.php';
 ?>
-<?php
-require_once 'components/aside.php';
-?>
 
 <div class="item">
-<h1>Welcome!</h1>
-<p>This is the main content area.</p>
-</div>
+    <h1>Welcome!</h1>
+    <p>This is the main content area.</p>
+</div>  
+
 
 <?php
 require_once 'components/footer.php';
